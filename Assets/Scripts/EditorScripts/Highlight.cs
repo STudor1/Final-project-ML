7475cf1893381e07c5ec86bce965c13a72d9e8a2
STudor1 <tudor122stefan@gmail.com>
2023-02-26 14:24:59 +0000

    Added highlight to the UI objects

---
+++ Assets/Scripts/EditorScripts/Highlight.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlight : MonoBehaviour
+{
+    [SerializeField] private GameObject highlight;
+
+    void OnMouseEnter()
+    {
+        highlight.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        highlight.SetActive(false);
+    }
+}
