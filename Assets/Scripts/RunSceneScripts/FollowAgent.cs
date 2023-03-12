644b3f373a35f85897c41f32189186e3e1a46a13
STudor1 <tudor122stefan@gmail.com>
2023-03-12 11:17:04 +0000

    Got rid of some scripts that were not being used

--- Assets/Scripts/RunSceneScripts/FollowAgent.cs
+++ Assets/Scripts/RunSceneScripts/FollowAgent.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraStartPosition : MonoBehaviour
+public class FollowAgent : MonoBehaviour
 {
-
     //I will use this to get the agent and get the camera to follow it
     //camera has to always be behind the agent and you set the rotation of the camera to follow the agent
     //set cam y to 90 if agent is placed on the left side wall
