71e2a27ac4b0d8aa0bdb21a10a87eae1343147ff
STudor1 <tudor122stefan@gmail.com>
2023-03-12 12:37:43 +0000

    Camera follows the agent

--- Assets/Scripts/RunSceneScripts/FollowAgent.cs
+++ Assets/Scripts/RunSceneScripts/FollowAgent.cs
@@ -4,11 +4,40 @@ using UnityEngine;
 
 public class FollowAgent : MonoBehaviour
 {
+    [SerializeField] private Transform agent; //when we want anything to do with position we need the transform not the game object
+    private bool isAgent = false;
+    private float xRot = 10f; //cam angles by 10 in x axis
+    private float yRot = 90f; //cam angles by 10 in x axis
+    //We want z = 0 meaning it is on the agent, y 0.75 so it sits a little above the agent and x -1 so it is behind it 
+    private Vector3 offset = new Vector3(-1f, 0.75f, 0f);
+
     //I will use this to get the agent and get the camera to follow it
     //camera has to always be behind the agent and you set the rotation of the camera to follow the agent
     //set cam y to 90 if agent is placed on the left side wall
-    private void Start()
+    private void Awake()
     {
-        //set the camera pos here 
+        transform.localRotation = Quaternion.Euler(xRot, yRot, 0f);
+    }
+
+    void Update()
+    {
+        if(isAgent == false)
+        {
+            Debug.Log("Finding agent");
+            findAgent();
+        }
+        else
+        {
+            transform.position = agent.position + offset; //x, y, z 
+        }
+    }
+
+    //Finds the agent in the scene
+    //There's always going to be an agent (need to add this later)
+    private void findAgent()
+    {
+        agent = GameObject.Find("Agent3D(Clone)").transform;
+        Debug.Log("Agent found");
+        isAgent = true;
     }
 }
