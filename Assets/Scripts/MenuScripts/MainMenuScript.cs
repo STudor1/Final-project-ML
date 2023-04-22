ebb981a2b622bc832601ac7f797dae7471226cc5
STudor1 <tudor122stefan@gmail.com>
2023-04-22 22:16:54 +0100

    Check if grid is between given values

--- Assets/Scripts/MenuScripts/MainMenuScript.cs
+++ Assets/Scripts/MenuScripts/MainMenuScript.cs
@@ -1,23 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 
 public class MainMenuScript : MonoBehaviour
 {
-    public static int width, height;
+    [SerializeField] private int maxWidthSize;
+    [SerializeField] private int maxHeightSize;
+    [SerializeField] private int minWidthSize;
+    [SerializeField] private int minHeightSize;
+    public static int width;
+    public static int height;
     public TMP_InputField xField;
     public TMP_InputField yField;
-    public void StartProgram() {
+    private bool isWidthBetween;
+    private bool isHeightBetween;
+
+    private void StartProgram() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    //check for ints later
+    //This will be called to get the user input from the fields 
     public void GetUserInput() {
         width = int.Parse(xField.text.ToString());
         height = int.Parse(yField.text.ToString());
-        //Debug.Log("1x " + width);
-        //Debug.Log("1y " + height);
+
+        CheckWidth(width); //check if width is between given values
+        CheckHeight(height); //check if height is between given values
+
+        //if both are between values move on to the next scene
+        if (isWidthBetween && isHeightBetween)
+        {
+            StartProgram();
+        }
+    }
+
+    private void CheckWidth(int width)
+    {
+        if (width > maxWidthSize || width < minWidthSize)
+        {
+            xField.text = "Enter a value between " + minWidthSize + " and " + maxWidthSize + ".";
+        }
+        else
+        {
+            isWidthBetween = true;
+        }
+    }
+
+    private void CheckHeight(int height)
+    {
+        if (height > maxHeightSize || height < minHeightSize)
+        {
+            yField.text = "Enter a value between " + minHeightSize + " and " + maxHeightSize + ".";
+        }
+        else
+        {
+            isHeightBetween = true;
+        }
     }
 }
